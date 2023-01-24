c56285f3541ddec2e66c9aff0fd774f388555356
Gray <gabrieliahern@gmail.com>
2023-01-24 13:44:55 -0800

    MotorDriver and EncoderReader Classes
    Created file template for motor driver class and encoder reader class

---
+++ src/motor_driver.py
@@ -0,0 +1,26 @@
+class MotorDriver:
+    """! 
+    This class implements a motor driver for an ME405 kit. 
+    """
+
+    def __init__ (self, en_pin, in1pin, in2pin, timer):
+        """! 
+        Creates a motor driver by initializing GPIO
+        pins and turning off the motor for safety. 
+        @param en_pin (There will be several pin parameters)
+        """
+        print ("Creating a motor driver")
+
+    def set_duty_cycle (self, level):
+        """!
+        This method sets the duty cycle to be sent
+        to the motor to the given level. Positive values
+        cause torque in one direction, negative values
+        in the opposite direction.
+        @param level A signed integer holding the duty
+               cycle of the voltage sent to the motor 
+        """
+        print (f"Setting duty cycle to {level}")
+        
+if __name__ == '__main__'
+# Section for testing code?
